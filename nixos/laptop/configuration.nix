7fbb65504caf17e549ea3a3618d95c35eed9f99e
Grant Ammons <gammons@gmail.com>
2021-11-08 18:29:12 -0500

    decruft and add better comments

--- nixos/laptop/configuration.nix
+++ nixos/laptop/configuration.nix
@@ -1,12 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
+      <nixos-hardware/dell/xps/15-9500>
       ./hardware-configuration.nix
     ];
 
@@ -29,40 +26,27 @@
     };
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "America/New_York";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  ####################################
+  # Networking setup
+  ####################################
+
   networking.useDHCP = false;
-  #networking.interfaces.enp57s0u1u2.useDHCP = true;
   networking.interfaces.wlp0s20f3.useDHCP = false;
-
   networking.networkmanager.enable = true;
-  networking.wireless.enable = false;
-  # networking.wireless.interfaces = ["wlp2s0f3"];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  ####################################
+  # X11
+  ####################################
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
     xkbOptions = "caps:ctrl_modifier";
     autoRepeatDelay = 200;
     autoRepeatInterval = 35;
+
     displayManager = {
       defaultSession = "none+i3";
       gdm = {
@@ -70,38 +54,27 @@
         wayland = false;
       };
     };
+
+    libinput = {
+      enable = true;
+
+      touchpad = {
+        disableWhileTyping = true;
+        tapping = false;
+        naturalScrolling = true;
+        clickMethod = "clickfinger";
+      };
+    };
+
     windowManager.i3.enable = true;
   };
 
-  # enable kbconfig in console terminals outside of X
-  console.useXkbConfig = true;
-
   nixpkgs.config.allowUnfree = true;
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput = {
-    enable = true;
-
-    touchpad = {
-      disableWhileTyping = true;
-      tapping = false;
-      naturalScrolling = true;
-      clickMethod = "clickfinger";
-    };
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.grant = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
