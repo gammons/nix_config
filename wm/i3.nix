e74de4d74e8abb57c0ea4643458f8097855883e4
Grant Ammons <gammons@gmail.com>
2021-11-09 06:12:40 -0500

    get most of my i3 config working

--- wm/i3.nix
+++ wm/i3.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 let
   mod = "Mod1";
+  term = "kitty";
 in {
   xsession.enable = true;
   xsession.windowManager.i3 = {
@@ -10,7 +11,7 @@ in {
 
     config = rec {
       modifier = "Mod1";
-      terminal = "kitty";
+      terminal = term;
 
       focus.followMouse = false;
 
@@ -27,6 +28,12 @@ in {
 
         "${modifier}+f" = "fullscreen toggle";
         "${modifier}+p" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+
+        "${modifier}+F11" = "exec --no-startup-id brightnessctl set 5%-";
+        "${modifier}+F12" = "exec --no-startup-id brightnessctl set +5%";
+
+        "${modifier}+u" = ''[title="dropdown-terminal"] scratchpad show; move position center'';
+        "${modifier}+Shift+u" = ''exec --no-startup-id ${term} --title=dropdown-terminal'';
       };
 
       startup = [
@@ -45,11 +52,33 @@ in {
           always = false;
           notification = false;
         }
+        {
+          command = "setxkbmap -option caps:ctrl_modifier";
+          always = false;
+          notification = false;
+        }
       ];
     };
 
     extraConfig = ''
       smart_gaps on
+      hide_edge_borders both
+
+      bindsym --release F12 exec --no-startup-id flameshot gui
+
+      for_window [class="zoom"] floating enable
+      for_window [class="zoom"] sticky enable
+      for_window [class="VirtualBox"] floating enable
+      for_window [class="Gnome-control-center"] floating enable
+
+      assign [class="Slack"] 2
+      assign [class="Spotify"] 4
+      for_window [title="dropdown-terminal"] floating enable
+      for_window [title="dropdown-terminal"] resize set 925 700
+      for_window [title="dropdown-terminal"] move position center
+      for_window [title="dropdown-terminal"] move scratchpad
+      for_window [title="Meet - "] sticky enable
+      exec --no-startup-id ${term} --title=dropdown-terminal
     '';
   };
 }
