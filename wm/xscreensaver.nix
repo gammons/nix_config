109476f3a7a1f1bb81cf5b758ffc6f7695f07457
Grant Ammons <gammons@gmail.com>
2021-11-11 20:14:03 -0500

    shorten time to screensaver

--- wm/xscreensaver.nix
+++ wm/xscreensaver.nix
@@ -5,6 +5,7 @@
       lock = true;
       dpmsEnabled = true;
       dpmsQuickOff = false;
+      timeout = "0:05:00";
       dpmsStandby = "0:20:00";
       dpmsSuspend = "0:30:00";
       mode = "random";
