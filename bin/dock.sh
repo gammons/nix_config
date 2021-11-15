68daddee61924d23f83d35c710925813d441824d
Grant Ammons <gammons@gmail.com>
2021-11-15 16:08:20 -0500

    need to restart setxkbmap after plugging something in

--- bin/dock.sh
+++ bin/dock.sh
@@ -2,4 +2,5 @@
 
 cp ~/.config/nixpkgs/wm/dpi-desktop.nix ~/.config/nixpkgs/wm/dpi.nix
 autorandr --load docked
+systemctl --user restart setxkbmap
 home-manager switch
--- bin/mobile.sh
+++ bin/mobile.sh
@@ -2,5 +2,6 @@
 
 cp ~/.config/nixpkgs/wm/dpi-laptop.nix ~/.config/nixpkgs/wm/dpi.nix
 autorandr --load mobile
+systemctl --user restart setxkbmap
 home-manager switch
 
