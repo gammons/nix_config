b4452d87d8e9fbeba26e0979485447dfc797d32b
Grant Ammons <gammons@gmail.com>
2021-11-17 08:32:50 -0500

    let the stonk handle failures

--- wm/polybar/stonks.rb
+++ wm/polybar/stonks.rb
@@ -3,9 +3,12 @@ require 'uri'
 require 'json'
 
 class Stonks
+  attr_reader :price
+
   def initialize(api_key, stock)
     @api_key = api_key
     @stock = stock
+    @price = nil
   end
 
   def fetch
@@ -13,11 +16,16 @@ class Stonks
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
-    @price = result[0]["price"].to_f
-    @percent_change = result[0]["change"].to_f
+
+    if response.code.start_with?("2")
+      @price = result[0]["price"].to_f
+      @percent_change = result[0]["change"].to_f
+    end
   end
 
   def to_s
+    return unless @price
+
     arrow = @percent_change > 0 ? "↑" : "↓"
     puts "#{@stock}: $#{"%.1f" % @price} (#{arrow}#{"%.1f" % @percent_change}%)"
   end
@@ -27,5 +35,4 @@ begin
   stock = Stonks.new("63193cb2454152fc84b76ed35c391f3b", ARGV[0])
   stock.fetch
   puts stock.to_s
-rescue nil
 end
