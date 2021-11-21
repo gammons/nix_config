9868dfc0b4c4b73185532b50d79c2dd75fd9b539
Grant Ammons <gammons@gmail.com>
2021-11-21 06:02:23 -0500

    set the correct xcursor size

--- wm/sway.nix
+++ wm/sway.nix
@@ -100,7 +100,7 @@ in {
       smart_gaps on
       hide_edge_borders both
 
-      seat seat0 xcursor_theme Adwaita 48
+      seat seat0 xcursor_theme Adwaita 24
 
       bindsym --release F12 exec --no-startup-id flameshot gui
 
